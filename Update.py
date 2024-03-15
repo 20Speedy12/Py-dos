d3406ac9b77c629518a91d97091e5428b5bb4401
20Speedy12 <163233071+20Speedy12@users.noreply.github.com>
2024-03-14 20:46:37 -0500

    Create Update.py

---
+++ Update.py
@@ -0,0 +1,8 @@
+#python -m pip install requests
+#import requests
+#print("checking for updates")
+
+
+
+#response = requests.get("https://api.github.com/repos/20Speedy12/Some-what-Dos-python/releases/latest")
+#print(response.json()["name"])
